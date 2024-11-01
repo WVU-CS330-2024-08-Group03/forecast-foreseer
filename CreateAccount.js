2798e869762a66bd6f617b166fbd92a5659d2cf3
nat00008 <nat00008@mix.wvu.edu>
2024-11-01 11:20:14 -0400

    Age Validation and Username and Email for Create Account

--- CreateAccount.js
+++ CreateAccount.js
@@ -1,12 +1,42 @@
-﻿function ageCount() {
-    let date = new Date();
-    let dob = document.getElementById("birthDate");
-    let year = date.getFullYear() - dob.getFullYear();
-    let month = date.getMonth() - dob.getMonth();
-    let day = date.getDay() - dob.getDay();
-    let ageInDays = year * 365 + month * 30 + day;
-    const minAge = 4745;
-    if (minAge > ageInDays) {
-        $("#isAge").removeClass("notVisible");
+﻿$(function(){
+    let date;
+    let dob;
+    let year;
+    let month;
+    let day;
+    function ageCheck() {
+        date = new Date();
+        monthString = $("#birthDate").val().substring(5,7);
+        yearString = $("#birthDate").val().substring(0,4);
+        dayString = $("#birthDate").val().substring(8);
+        dateString = yearString+"/"+monthString+"/"+dayString;
+        dob = new Date(dateString);
+        year = date.getFullYear() - dob.getFullYear();
+        month = date.getMonth() - dob.getMonth();
+        day = date.getDate() - dob.getDate();
+        if (13>year) {
+            $("#isAge").css({display: 'block'});
+        }
+        else if (13==year) {
+            if (month<0) {
+                $("#isAge").css({display: 'block'});
+            }
+            else if (month==0) {
+                if(day<0) {
+                    $("#isAge").css({display: 'block'});
+                }
+                else {
+                    $("#isAge").css({display: 'none'});
+                }
+            }
+            else {
+                $("#isAge").css({display: 'none'});
+            }
+        }
+        else {
+            $("#isAge").css({display: 'none'});
+        }
+       //console.log("Birthdate changed");
     }
-}
+    $("#birthDate").on("change", ageCheck);
+});
