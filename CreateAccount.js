2654e5a88745f144a8e1420ce4ec9ee1972e9963
nat00008 <nat00008@mix.wvu.edu>
2024-11-04 10:13:57 -0500

    Required Fields, Modified Styles, and Javascript
    All fields are currently required. Form will not submit without all fields filled. Form will also not submit if entered date is less than minimum age. Added styles for message about not meeting minimum age.

--- CreateAccount.js
+++ CreateAccount.js
@@ -1,13 +1,30 @@
 ﻿$(function(){
+    /**
+     * Variables for date, dob (Date of Birth), year, month, and day
+     * comparisons between date and dob. Also creates variables for month, year, and day strings
+     */
     let date;
     let dob;
     let year;
     let month;
     let day;
+    let monthString;
+    let yearString;
+    let dayString;
+    let meetsAge = false;
+
+    /**
+     * Checks age of user. Compares year to wanted age. Gets more sepcific, if values == a given.
+     * Checks month if year is equal, checks day if month is equal.
+     */
     function ageCheck() {
+        //Stores a date into the date variable
         date = new Date();
+        //Gets substring of birth date input
         monthString = $("#birthDate").val().substring(5,7);
+        //Gets substring of birth date input
         yearString = $("#birthDate").val().substring(0,4);
+        //
         dayString = $("#birthDate").val().substring(8);
         dateString = yearString+"/"+monthString+"/"+dayString;
         dob = new Date(dateString);
@@ -16,27 +33,41 @@
         day = date.getDate() - dob.getDate();
         if (13>year) {
             $("#isAge").css({display: 'block'});
+            meetsAge = false;
         }
         else if (13==year) {
             if (month<0) {
                 $("#isAge").css({display: 'block'});
+                meetsAge = false;
             }
             else if (month==0) {
                 if(day<0) {
                     $("#isAge").css({display: 'block'});
+                    meetsAge=false;
                 }
                 else {
                     $("#isAge").css({display: 'none'});
+                    meetsAge=true;
                 }
             }
             else {
                 $("#isAge").css({display: 'none'});
+                meetsAge=true;
             }
         }
         else {
             $("#isAge").css({display: 'none'});
+            meetsAge=true;
         }
        //console.log("Birthdate changed");
     }
     $("#birthDate").on("change", ageCheck);
+    
+
+    $("#userInfoForm").on("submit", function(){
+        if (meetsAge==false){
+            console.log("Form failed to submit")
+            event.preventDefault();
+        }
+    });
 });
